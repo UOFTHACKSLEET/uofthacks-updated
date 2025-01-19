054eb1c3d164301123cbbbe5880ff8240b7efda1
amanduhhhh <axi@uwaterloo.ca>
2025-01-19 04:41:21 -0500

    loader animatino for the hahas

--- behavioural_leet/src/Layout.jsx
+++ behavioural_leet/src/Layout.jsx
@@ -1,22 +1,39 @@
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar/Sidebar";
+import "./Layout.css";
 
 function Layout() {
+  const [loaderActive, setLoaderActive] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaderActive(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <Sidebar />
-      <div
-        className="page"
-        style={{
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Outlet />
-      </div>
-    </div>
+    <>
+      {loaderActive && <span className="loader"></span>}
+      {!loaderActive && (
+        <div className="App">
+          <Sidebar />
+          <div
+            className="page"
+            style={{
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Outlet />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
